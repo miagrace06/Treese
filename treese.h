de4fd462767cef088291f846bf3fb41b3d44f4c5
miagrace06 <mcampanella@ucsb.edu>
2025-07-17 12:41:57 -0700

    Update treese.h

--- treese.h
+++ treese.h
@@ -1,5 +1,6 @@
 #ifndef TREESE_H
 #define TREESE_H
+
 #include <string>
 #include <cstddef> 
 #include <stdexcept> 
@@ -11,20 +12,20 @@ private:
         bool is_leaf;
         size_t length;
         size_t height;
-        
         std::string data;
-        
         Node* left;
         Node* right;
-        
-        Node(const std::string& str) : is_leaf(true), length(str.length()), 
-                                       height(1), data(str), left(nullptr), right(nullptr) {}
-        
-        Node(Node* l, Node* r) : is_leaf(false), left(l), right(r), data("") {
+
+        Node(const std::string& str)
+            : is_leaf(true), length(str.length()), height(1),
+              data(str), left(nullptr), right(nullptr) {}
+
+        Node(Node* l, Node* r)
+            : is_leaf(false), left(l), right(r), data("") {
             length = (l ? l->length : 0) + (r ? r->length : 0);
             height = 1 + std::max(l ? l->height : 0, r ? r->height : 0);
         }
-        
+
         ~Node() {
             if (!is_leaf) {
                 delete left;
@@ -32,17 +33,19 @@ private:
             }
         }
     };
-    
+
     Node* root;
-    
+
     Node* copyTree(Node* node);
     void printHelper(Node* node) const;
+
     Node* balance(Node* node);
     size_t getHeight(Node* node);
     int getBalance(Node* node);
-    Node* rotateRight(Node* node);
-    Node* rotateLeft(Node* node);
+    Node* rotateRight(Node* y);
+    Node* rotateLeft(Node* x);
     void updateHeight(Node* node);
+    void updateLength(Node* node);
 
 public:
     Treese();
@@ -50,10 +53,11 @@ public:
     Treese(const Treese& other);
     Treese& operator=(const Treese& other);
     ~Treese();
+
     size_t length() const;
     char at(size_t index) const;
     void print() const;
-    
-#endif // TREESE_H
     Treese concat(const Treese& other) const;
 };
+
+#endif // TREESE_H
